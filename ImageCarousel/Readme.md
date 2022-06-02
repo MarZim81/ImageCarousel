abfe7995281ec3656b88b8012a074e39c1cc78ab
Markus <58768032+DarkSpawn81@users.noreply.github.com>
2022-06-02 16:15:39 +0200

    Projektdateien hinzufügen.

---
+++ ImageCarousel/Readme.md
@@ -0,0 +1,16 @@
+﻿How to install:
+***************
+
+From the zip package:
+	Unzip all the files into a directory you want then launch "imagecarousel.exe".
+	Maybe you have to install the Microsoft .Net 6 Desktop Runtime (https://dotnet.microsoft.com/en-us/download/dotnet/6.0)
+
+Disclaimer:
+***********
+USE AT YOUR OWN RISK!
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+	
+Author:
+*******
+
+Markus Zimmermann - imagecarousel@gmx.de
---
+++ ImageCarousel/frmMain.Designer.cs
@@ -0,0 +1,396 @@
+﻿namespace ImageCarousel
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnSelFolder = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.timerMain = new System.Windows.Forms.Timer(this.components);
+            this.picMain = new System.Windows.Forms.PictureBox();
+            this.timerPic = new System.Windows.Forms.Timer(this.components);
+            this.progBar = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.btnSkip = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblActual = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbStopAfter = new System.Windows.Forms.ComboBox();
+            this.cmbInterval = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chkSubFolders = new System.Windows.Forms.CheckBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.picMain)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSelFolder
+            // 
+            this.btnSelFolder.BackColor = System.Drawing.Color.DimGray;
+            this.btnSelFolder.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btnSelFolder.Location = new System.Drawing.Point(12, 35);
+            this.btnSelFolder.Name = "btnSelFolder";
+            this.btnSelFolder.Size = new System.Drawing.Size(132, 33);
+            this.btnSelFolder.TabIndex = 0;
+            this.btnSelFolder.Text = "Select Image-Folder";
+            this.btnSelFolder.UseVisualStyleBackColor = false;
+            this.btnSelFolder.Click += new System.EventHandler(this.btnSelFolder_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.Gray;
+            this.btnStart.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btnStart.Location = new System.Drawing.Point(12, 190);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(261, 29);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // timerMain
+            // 
+            this.timerMain.Interval = 10000;
+            this.timerMain.Tick += new System.EventHandler(this.timerMain_Tick);
+            // 
+            // picMain
+            // 
+            this.picMain.BackColor = System.Drawing.Color.Black;
+            this.picMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picMain.Location = new System.Drawing.Point(0, 0);
+            this.picMain.Name = "picMain";
+            this.picMain.Size = new System.Drawing.Size(800, 555);
+            this.picMain.TabIndex = 0;
+            this.picMain.TabStop = false;
+            // 
+            // timerPic
+            // 
+            this.timerPic.Interval = 1000;
+            this.timerPic.Tick += new System.EventHandler(this.timerPic_Tick);
+            // 
+            // progBar
+            // 
+            this.progBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progBar.Location = new System.Drawing.Point(0, 49);
+            this.progBar.Name = "progBar";
+            this.progBar.Size = new System.Drawing.Size(794, 15);
+            this.progBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progBar.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Location = new System.Drawing.Point(224, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Actual image:";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(300, 34);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Panel1.Controls.Add(this.progBar);
+            this.splitContainer1.Panel1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.picMain);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 630);
+            this.splitContainer1.SplitterDistance = 71;
+            this.splitContainer1.TabIndex = 5;
+            // 
+            // btnSkip
+            // 
+            this.btnSkip.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSkip.BackColor = System.Drawing.Color.Gray;
+            this.btnSkip.Location = new System.Drawing.Point(15, 8);
+            this.btnSkip.Name = "btnSkip";
+            this.btnSkip.Size = new System.Drawing.Size(75, 23);
+            this.btnSkip.TabIndex = 8;
+            this.btnSkip.Text = "Skip";
+            this.btnSkip.UseVisualStyleBackColor = false;
+            this.btnSkip.Click += new System.EventHandler(this.btnSkip_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label2.Location = new System.Drawing.Point(9, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Total images in folder:";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.lblTotal.Location = new System.Drawing.Point(138, 10);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(13, 15);
+            this.lblTotal.TabIndex = 6;
+            this.lblTotal.Text = "0";
+            // 
+            // lblActual
+            // 
+            this.lblActual.AutoSize = true;
+            this.lblActual.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.lblActual.Location = new System.Drawing.Point(305, 10);
+            this.lblActual.Name = "lblActual";
+            this.lblActual.Size = new System.Drawing.Size(13, 15);
+            this.lblActual.TabIndex = 5;
+            this.lblActual.Text = "0";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.cmbStopAfter);
+            this.groupBox1.Controls.Add(this.cmbInterval);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.groupBox1.Location = new System.Drawing.Point(12, 90);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(261, 94);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Options";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(155, 57);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "images";
+            // 
+            // cmbStopAfter
+            // 
+            this.cmbStopAfter.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbStopAfter.FormattingEnabled = true;
+            this.cmbStopAfter.Location = new System.Drawing.Point(75, 54);
+            this.cmbStopAfter.Name = "cmbStopAfter";
+            this.cmbStopAfter.Size = new System.Drawing.Size(74, 23);
+            this.cmbStopAfter.TabIndex = 3;
+            // 
+            // cmbInterval
+            // 
+            this.cmbInterval.DisplayMember = "Description";
+            this.cmbInterval.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbInterval.FormattingEnabled = true;
+            this.cmbInterval.Location = new System.Drawing.Point(75, 27);
+            this.cmbInterval.Name = "cmbInterval";
+            this.cmbInterval.Size = new System.Drawing.Size(121, 23);
+            this.cmbInterval.TabIndex = 2;
+            this.cmbInterval.ValueMember = "Seconds";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 15);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Stop after:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 15);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Interval:";
+            // 
+            // chkSubFolders
+            // 
+            this.chkSubFolders.AutoSize = true;
+            this.chkSubFolders.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.chkSubFolders.Location = new System.Drawing.Point(150, 43);
+            this.chkSubFolders.Name = "chkSubFolders";
+            this.chkSubFolders.Size = new System.Drawing.Size(123, 19);
+            this.chkSubFolders.TabIndex = 7;
+            this.chkSubFolders.Text = "Include subfolders";
+            this.chkSubFolders.UseVisualStyleBackColor = true;
+            this.chkSubFolders.CheckedChanged += new System.EventHandler(this.chkSubFolders_CheckedChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1112, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.infoToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(24, 20);
+            this.toolStripMenuItem1.Text = "?";
+            // 
+            // infoToolStripMenuItem
+            // 
+            this.infoToolStripMenuItem.Name = "infoToolStripMenuItem";
+            this.infoToolStripMenuItem.Size = new System.Drawing.Size(104, 22);
+            this.infoToolStripMenuItem.Text = "Info...";
+            this.infoToolStripMenuItem.Click += new System.EventHandler(this.infoToolStripMenuItem_Click);
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.splitContainer2.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.label2);
+            this.splitContainer2.Panel1.Controls.Add(this.lblTotal);
+            this.splitContainer2.Panel1.Controls.Add(this.label1);
+            this.splitContainer2.Panel1.Controls.Add(this.lblActual);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.btnSkip);
+            this.splitContainer2.Size = new System.Drawing.Size(800, 43);
+            this.splitContainer2.SplitterDistance = 700;
+            this.splitContainer2.TabIndex = 9;
+            // 
+            // frmMain
+            // 
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(1112, 676);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.chkSubFolders);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnSelFolder);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Image Carousel";
+            ((System.ComponentModel.ISupportInitialize)(this.picMain)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnSelFolder;
+        private Button btnStart;
+        private System.Windows.Forms.Timer timerMain;
+        private PictureBox picMain;
+        private System.Windows.Forms.Timer timerPic;
+        private ProgressBar progBar;
+        private Label label1;
+        private SplitContainer splitContainer1;
+        private GroupBox groupBox1;
+        private Label label3;
+        private CheckBox chkSubFolders;
+        private Label lblActual;
+        private Label label5;
+        private ComboBox cmbStopAfter;
+        private ComboBox cmbInterval;
+        private Label label4;
+        private Label lblTotal;
+        private Label label2;
+        private Button btnSkip;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripMenuItem toolStripMenuItem1;
+        private ToolStripMenuItem infoToolStripMenuItem;
+        private SplitContainer splitContainer2;
+    }
+}
