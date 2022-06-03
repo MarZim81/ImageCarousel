f0b84ba73a317ad5d6c6e28b40c9f3e70655d73b
Markus <58768032+DarkSpawn81@users.noreply.github.com>
2022-06-03 11:47:15 +0200

    Update - changed from Forms to WPF App

---
+++ ImageCarouselApp/InfoWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace ImageCarousel;
+
+public partial class InfoWindow : Window
+{
+    public InfoWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void BtnClose_OnClick(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+}
